16001dc73fc568405968626549afdca8759fa826
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-02-06 00:21:14 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+
+let mode = "dark";
+let color;
+ if (mode==dark);
+{
+    color="black";
+ console.log("visible");
+} 
+ 
+ if (mode==dark)
+
+ {
+    color="white";
+ console.log("invisible");
+}
